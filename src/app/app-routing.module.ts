6c6a47851624d0866ac5c0cad57a12ce126e1f45
vpavelko <vpavelko@softjourn.com>
2023-08-09 16:01:34 +0300

    add routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { SchedulerFormComponent } from './components/scheduler-form/scheduler-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'scheduler-form', component: SchedulerFormComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
